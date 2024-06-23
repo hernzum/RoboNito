cb98586ec05bf2c0a47054e2b4cb632a67d059cf
Hernán M <325008+hernzum@users.noreply.github.com>
2024-06-24 00:41:07 +0200

    Update robonitoIR.ino

--- robonitoIR.ino
+++ robonitoIR.ino
@@ -1,127 +1,117 @@
-//IRobonito V 2
-//IR remote controlled Robot 
-// (c) hernan Moya
+/*
+ * IRobonito V2
+ * IR remote controlled Robot
+ * (c) 2020 Hernan Moya
+ *
+ * This sketch controls a robot using an IR remote. It defines actions such as
+ * moving forward, moving backward, turning left, turning right, and stopping.
+ * It listens for specific IR remote codes to trigger these actions.
+ */
+
 #include <Servo.h>
 #include <IRremote.h>
 
+// Pin definitions
 int RECV_PIN = 4;
-//int ledPin = 5;
-//int button = 7;
 int servoRightPin = 8;
 int servoLeftPin = 9;
-//const int numberOfKeys = 5; // 5 keys are learned (0 through 4)
-//long irKeyCodes[numberOfKeys]; // holds the codes for each key
+
+// Servo objects
 Servo servoRight;
 Servo servoLeft;
+
+// IR receiver object
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 /*
-* Declara las acciones del robot
-*/
-void moveForward()
-{
-servoLeft.write(180);
-servoRight.write(0);
-//digitalWrite(ledPin, HIGH);
-Serial.println("IRbonito. ADELANTE");
-}
-void moveBack()
-{
-servoLeft.write(0);
-servoRight.write(180);
-Serial.println("IRbonito. BACK");
-}
-void turnRight()
-{
-servoLeft.write(180);
-servoRight.write(180);
-Serial.println("IRbonito. DERECHA");
-}
-void turnLeft()
-{
-servoLeft.write(0);
-servoRight.write(0);
-Serial.println("IRbonito. IZQUIERDA");
+ * Function declarations for robot actions
+ */
+void moveForward();
+void moveBack();
+void turnRight();
+void turnLeft();
+void stopMoving();
 
-}
-
-void stopMoving()
-{
-servoLeft.write(90);
-servoRight.write(90);
-Serial.println("IRbonito. STOP");
-//digitalWrite(ledPin, LOW);
-}
 void setup()
 {
-Serial.begin(9600);
-servoRight.attach(servoRightPin);
-servoLeft.attach(servoLeftPin);
-//pinMode(ledPin, OUTPUT);
-//pinMode(button, INPUT);
-irrecv.enableIRIn(); // Start the ir receiver
-/*long irKeyCodes[numberOfKeys] = {
-839727, // forward 0 key
-803007, // stop 1 key
-831567, // turnLeft 2 key
-815247, //turnRight 3 key
-798927, //moveBack 4 key
-};
-*/
-//stopMoving();
-Serial.println("IRbonito. V3 (c) 2020");
-Serial.println("IRbonito. Listo para empezar....");
-stopMoving();
+  Serial.begin(9600);
+  servoRight.attach(servoRightPin);
+  servoLeft.attach(servoLeftPin);
+  irrecv.enableIRIn(); // Start the IR receiver
 
+  Serial.println("IRbonito V2 (c) 2020");
+  Serial.println("IRbonito: Ready to start...");
+  stopMoving(); // Ensure robot starts stopped
 }
 
 void loop()
 {
   moveForward();
-delay (5000);
-stopMoving();
-turnLeft();
-delay (1000);
-moveForward();
-delay (5000);
-turnRight();
-delay (1000);
-stopMoving();
-delay(1000);
-moveBack();
-delay (5000);
-stopMoving();
+  delay(5000);
+  stopMoving();
+  turnLeft();
+  delay(1000);
+  moveForward();
+  delay(5000);
+  turnRight();
+  delay(1000);
+  stopMoving();
+  delay(1000);
+  moveBack();
+  delay(5000);
+  stopMoving();
 
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, DEC);
 
- if (irrecv.decode(&results)) {
-   Serial.println(results.value, DEC);
-if(results.value == 839727)  // Benq Projector Remote codes
-  moveForward();   // IRObonito va adelante
+    // Check IR remote codes and perform actions accordingly
+    if (results.value == 839727) // Forward
+      moveForward();
+    else if (results.value == 803007) // Stop
+      stopMoving();
+    else if (results.value == 831567) // Turn left
+      turnLeft();
+    else if (results.value == 815247) // Turn right
+      turnRight();
+    else if (results.value == 798927) // Move back
+      moveBack();
+
     irrecv.resume(); // Receive the next value
-     if(results.value == 803007)
-     stopMoving(); // Para a IRobonito
-          
-    // irrecv.resume(); // Receive the next value
+  }
+}
 
-   if(results.value == 831567)
-     turnLeft();   // IRObonito gira a la izquierda
-      //irrecv.resume(); // Receive the next value
+void moveForward()
+{
+  servoLeft.write(180);
+  servoRight.write(0);
+  Serial.println("IRbonito: FORWARD");
+}
 
-   if(results.value == 815247)
-     turnRight();   // IRObonito gira a la Derecha
-        //irrecv.resume(); // Receive the next value
-   
-   if(results.value == 798927)
-     moveBack();   // IRObonito va hacia atrás
+void moveBack()
+{
+  servoLeft.write(0);
+  servoRight.write(180);
+  Serial.println("IRbonito: BACK");
+}
 
-   if(results.value == 839727)
-     moveForward();   // IRObonito va adelante
-  
-   if(results.value == 847887)
-     stopMoving();   // IRObonito stop
- 
+void turnRight()
+{
+  servoLeft.write(180);
+  servoRight.write(180);
+  Serial.println("IRbonito: RIGHT");
+}
 
+void turnLeft()
+{
+  servoLeft.write(0);
+  servoRight.write(0);
+  Serial.println("IRbonito: LEFT");
+}
 
- }
+void stopMoving()
+{
+  servoLeft.write(90);
+  servoRight.write(90);
+  Serial.println("IRbonito: STOP");
 }
